6936ff11391bb41b9200b14a0636f27bb59dfef2
BouzidiHalima <32882900+BouzidiHalima@users.noreply.github.com>
2017-10-17 22:40:52 +0200

    Voici le code, deux fonctions manquent

---
+++ LenghtNotEqualException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package VecteurHelp;
+
+/**
+ * @author pc
+ *
+ */
+public class LenghtNotEqualException extends Exception {
+	
+	public LenghtNotEqualException(){
+	    System.out.println("La taille des deux vecteurs n'est pas identique !");
+	    }
+
+}
