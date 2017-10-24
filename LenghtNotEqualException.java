c261741efa27634e166aa1c404e66cb29200e3ab
Yasmine98 <32877819+Yasmine98@users.noreply.github.com>
2017-10-24 16:54:22 +0200

    pour merge

--- LenghtNotEqualException.java
+++ LenghtNotEqualException.java
@@ -5,15 +5,9 @@ package VecteurHelper;
 
 /**
  * <b>La classe LenghtNotEqualException est une sous classe de Exception, elle contient l'unique methode LenghtNotEqualException.
- * elle afficherait un message indiquant à l'utilisateur que la taille des 2 vecteurs fournis est différente. </b>
+ * elle afficherait un message indiquant Ã  l'utilisateur que la taille des 2 vecteurs fournis est diffÃ©rente. </b>
  *
- */
-public class LenghtNotEqualException extends Exception
-/*L'exception déclenchée lorsque deux vecteurs n'ont pas la mème taille, sert pour le calcul de la somme de deux vecteurs.*/
-{
-	/**
-     * Cette Methode est le constructeur de l'exception.
-	  */
+
 	public LenghtNotEqualException(){
 	    super("La taille des deux vecteurs n'est pas identique !");
 	    }
